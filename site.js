bec82e8be60c10c20af4709d0cff78c0d75a57da
Gerald Wodni <gerald.wodni@gmail.com>
2014-09-21 15:12:21 +0200

    hello world

---
+++ site.js
@@ -0,0 +1,10 @@
+// theforth.net main include file, needs kern.js, see https://github.com/GeraldWodni/kern.js
+// (c)copyright 2014 by Gerald Wodni <gerald.wodni@gmail.com>
+
+module.exports = {
+    setup: function( k ) {
+        k.router.get("/c", function( req, res ) {
+            res.send( "theforth.net CONSOLE!" );
+        });
+    }
+};
